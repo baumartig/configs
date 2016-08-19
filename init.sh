d5dc0828e216e80bc1ef610900fd528f4b318d66
baumartig <david.braeutigam@gmail.com>
2016-08-19 16:50:09 +0200

    Update init.sh
    Create Folder before making the link

--- init.sh
+++ init.sh
@@ -8,5 +8,6 @@ CURRENT=$(pwd)
 
 #for i in $LINKS ; doi IFS=","; set $i; echo $1 $2; done
 echo "$LINKS" | while read src link ; do
+	mkdir -p $HOME/${link%/*}
 	ln -svf $CURRENT/$src $HOME/$link
 done
