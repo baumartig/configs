9c08980a8b81a379b7f5d33e6c564248cdab5ee9
David Bräutigam <david.braeutigam@gmail.com>
2016-08-21 21:20:18 +0200

    Fix link erstellung

--- init.sh
+++ init.sh
@@ -2,13 +2,13 @@
 
 # Zuordnung internes Verzeichniss zu Link Ziel
 
-LINKS="tmux/tmux_config .tmux.conf
-nvim/init.vim .config/nvim/init.vim
-zsh/zshrc .zshrc"
+LINKS="/tmux/tmux_config /.tmux.conf
+/nvim/init.vim /.config/nvim/init.vim
+/zsh/zshrc /.zshrc"
 CURRENT=$(pwd)
 
 #for i in $LINKS ; doi IFS=","; set $i; echo $1 $2; done
 echo "$LINKS" | while read src link ; do
-	mkdir -p $HOME/${link%/*}
-	ln -svf $CURRENT/$src $HOME/$link
+	mkdir -p $HOME${link%/*}
+	ln -svf $CURRENT$src $HOME$link
 done
